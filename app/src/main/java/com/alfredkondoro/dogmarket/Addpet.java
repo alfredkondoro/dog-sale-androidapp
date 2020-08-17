699352a6564ab3fc5ffc42364b8f1b951e4da24b
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-17 20:45:09 +0300

    version 0.2

---
+++ app/src/main/java/com/alfredkondoro/dogmarket/Addpet.java
@@ -0,0 +1,14 @@
+package com.alfredkondoro.dogmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addpet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_addpet);
+    }
+}
